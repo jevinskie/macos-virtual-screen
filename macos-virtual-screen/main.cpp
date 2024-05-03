c3284142bb3be08f95039615d1bf484a20164d2b
Jevin Sweval <jevinsweval@gmail.com>
2024-05-03 04:44:15 -0400

    Initial Commit

---
+++ macos-virtual-screen/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  macos-virtual-screen
+//
+//  Created by Jevin Sweval on 2024-05-03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
